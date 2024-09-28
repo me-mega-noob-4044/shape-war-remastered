2c6e8be34093affa5b4c74ae8ad6e1087d72d4ad
me-mega-noob-4044 <testtrolium@gmail.com>
2024-09-28 18:28:22 -0400

    added some comments

---
+++ client/src/worker.js
@@ -0,0 +1,3 @@
+// This is a placeholder
+// We will use web-workers to simluate a serverside environment
+// simluation is used to offload tasks like movement and damage calculations off of the main thread
