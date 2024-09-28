604c2d66d63143f3e209abdd97ee802a2089ba9c
me-mega-noob-4044 <testtrolium@gmail.com>
2024-09-28 10:11:32 -0400

    subdomain stuff

--- index.js
+++ index.js
@@ -10,16 +10,29 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    req.subdomain = req.headers.host.split(".")[0];
+
+    next();
+});
+
+app.get("/test", (req, res) => {
+    res.redirect("http://test.localhost:8080");
+});
+
 app.get("*", (req, res) => {
-    let { url } = req;
+    let { url, subdomain } = req;
+
     if (url.includes("/src/")) {
         res.sendFile(path.join(__dirname + url));
     } else if (url == "/script.js") {
         res.sendFile(path.join(__dirname + "/client/script.js"));
     } else if (url == "/main.css") {
         res.sendFile(path.join(__dirname + "/client/main.css"));
-    } else {
+    } else if (subdomain == "test")  {
         res.sendFile(path.join(__dirname + "/client/index.html"));
+    } else {
+        res.sendFile(path.join(__dirname + "/client/wip.html"));
     }
 });
 
