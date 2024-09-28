f033893ca0e59bc50e1069538664c791a2332905
me-mega-noob-4044 <testtrolium@gmail.com>
2024-09-28 18:29:33 -0400

    updated comments

--- client/src/worker.js
+++ client/src/worker.js
@@ -1,3 +1,3 @@
 // This is a placeholder
-// We will use web-workers to simluate a serverside environment
+// We will use web-workers to simluate a serverside environment (this is a single player game, websockets are not really needed)
 // simluation is used to offload tasks like movement and damage calculations off of the main thread
