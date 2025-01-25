b19bb8ac631f32e76b6b4e14609c63c6bb66800d
me-mega-noob-4044 <testtrolium@gmail.com>
2025-01-25 17:17:20 -0500

    task class

--- src/js/task.js
+++ src/js/task.js
@@ -1,1 +1,9 @@
-export default class Task { }
+export default class Task {
+    constructor(data) {
+        this.label = data.label;
+        this.description = data.description;
+        this.requirement = { ...data.requirement };
+        this.reward = { ...data.reward };
+        this.current = 0;
+    }
+}
