b829b8d1fa5e75855863de7a0513577b44f59179
me-mega-noob-4044 <testtrolium@gmail.com>
2025-05-23 14:25:20 -0400

    lootbox

--- src/js/lootboxItems.js
+++ src/js/lootboxItems.js
@@ -1,61 +1,93 @@
-
-class Item {
+export class LootboxItem {
 
     /**
      * @param {string} name 
-     * @param {string} type 
+     * @param {"money" | "shape" | "weapon" | "module" | "drone"} type 
      * @param {number} amount 
-     * @param {number} chance 
+     * @param {number} weight - Out of 100. This value is based on percentages (for example 50 -> 50%).
      */
 
-    constructor(name, type, amount, chance) {
+    constructor(name, type, amount, weight) {
         this.name = name;
         this.type = type;
         this.amount = amount;
-        this.chance = chance;
+        this.weight = weight;
 
         this.normalized = 0;
     }
-}
 
-/**
- * @param {Item[]} items 
- */
+    /**
+     * @param {LootboxItem[]} items 
+     */
 
-function normalizeChances(items) {
-    const total = items.map((a, b) => a.chance + b, 0);
+    static normalize(items) {
+        const total = items.reduce((sum, item) => sum + item.weight, 0);
 
-    for (let i = 0; i < items.length; i++) {
-        const item = items[i];
+        for (const item of items) {
+            item.normalized = item.weight / total;
+        }
+    }
 
-        item.normalized = item.chance / total;
+    /**
+     * @param {LootboxItem[]} items 
+     * @returns {LootboxItem}
+     */
+
+    static drawItem(items) {
+        const rand = Math.random();
+        let cumulative = 0;
+
+        for (const item of items) {
+            cumulative += item.normalized;
+
+            if (rand < cumulative) {
+                return item;
+            }
+        }
+
+        return null;
     }
 }
 
+// "gold" | "silver" | "platinum" | "keys" | "microchips" | "powercells"
+
 const lootboxItems = {
 
-    /** @type {Item[]} */
+    /** @type {LootboxItem[]} */
 
-    basic: [],
+    basic: [
+        new LootboxItem("gold", "money", 10, 80),
+        new LootboxItem("gold", "money", 5, 80),
+        new LootboxItem("gold", "money", 7, 80),
+        new LootboxItem("gold", "money", 15, 80),
 
-    /** @type {Item[]} */
+        new LootboxItem("silver", "money", 15e3, 80),
+        new LootboxItem("silver", "money", 5e3, 80),
+        new LootboxItem("silver", "money", 2e3, 80),
+
+        new LootboxItem("keys", "money", 7, 80),
+        new LootboxItem("keys", "money", 5, 80),
+        new LootboxItem("keys", "money", 2, 80),
+        new LootboxItem("keys", "money", 10, 80),
+        new LootboxItem("keys", "money", 10, 15)
+    ],
+
+    /** @type {LootboxItem[]} */
 
     intermediate: [],
 
-    /** @type {Item[]} */
+    /** @type {LootboxItem[]} */
 
     master: [],
 
-    /** @type {Item[]} */
+    /** @type {LootboxItem[]} */
 
     champion: []
 };
 
-normalizeChances(lootboxItems.basic);
-normalizeChances(lootboxItems.intermediate);
-normalizeChances(lootboxItems.master);
-normalizeChances(lootboxItems.champion);
-
-console.log(lootboxItems);
+LootboxItem.normalize(lootboxItems.basic);
+LootboxItem.normalize(lootboxItems.intermediate);
+LootboxItem.normalize(lootboxItems.master);
+LootboxItem.normalize(lootboxItems.champion);
 
 export default lootboxItems;
