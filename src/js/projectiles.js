8a76d072622c82f9c1b7faee14338a91e34abc7f
me-mega-noob-4044 <testtrolium@gmail.com>
2025-05-26 14:13:32 -0400

    new rocket projectile

--- src/js/projectiles.js
+++ src/js/projectiles.js
@@ -1,12 +1,22 @@
 // Name attribute is not used in the code
 // but is for dev purposes
 
+/**
+ * @type {{ name: string; type: "normal" | "rocket"; speed: number; scale: number; imageSource: string; }[]}
+ */
+
 const projectiles = [{
     name: "normal projectile",
     type: "normal",
-    speed: 3.5,
+    speed: 4,
     scale: 20,
     imageSource: "../src/media-files/projectiles/normal.png"
+}, {
+    name: "rockets",
+    type: "rocket",
+    speed: 3,
+    scale: 25,
+    imageSource: "../src/media-files/projectiles/rocket.png"
 }];
 
 export default projectiles;
